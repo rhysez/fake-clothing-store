88ab2653f275deec42db86ede302e7d767e7207d
rhysez <121772484+rhysez@users.noreply.github.com>
2023-10-17 22:46:19 +0100

    add additional hero content to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import Nav from './components/Nav'
 
 function App() {
-  
+
+  return (
+    <>
+      <Nav />
+      <div className='hero-container'>
+        <h1 className='hero-title'>WELCOME TO FRESH</h1>
+        <h3 className='hero-subtitle'>Some other text</h3>
+        <button className='hero-shop-now'>Shop now</button>
+      </div>
+    </>
+  )
 }
 
 export default App
