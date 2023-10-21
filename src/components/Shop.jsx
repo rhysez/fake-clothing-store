840bf4709218696ef4cbd396302b713803708117
rhysez <121772484+rhysez@users.noreply.github.com>
2023-10-21 20:44:38 +0100

    add conditional return statement in Shop to only render content when promise is fulfilled

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -10,30 +10,43 @@ function Shop() {
     const [itemData, setItemData] = useState(null)
 
     useEffect(() => {
-        fetch('https://fakestoreapi.com/products')
-        .then((response) => response.json())
-        .then((response) => setItemData(response))
-        .then(console.log(itemData))
-        .catch((error) => console.log(error))
+        async function fetchData() {
+            try {
+                const response = await fetch('https://fakestoreapi.com/products', { mode: 'cors' })
+                const responseJson = await response.json()
+                setItemData(responseJson)
+            } catch {
+                throw new Error('Could not retrieve product')
+            }
+        }
+        fetchData()
     }, [])
 
-  return (
-    <>
-      <Nav></Nav>
-      <h1 style={{ marginTop: "10rem", textAlign: "center", fontSize: "4rem" }}>
-        SHOP
-      </h1>
-      <div
-        className="shop-items-container"
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(4, 1fr)",
-          gridTemplateRows: "repeat(4, 1fr)",
-          gap: "2rem",
-        }}
-      ></div>
-    </>
-  );
+  if (itemData === null) {
+    return (
+        <h2 style={{marginTop: '10rem'}}>Loading items...</h2>
+    )
+  } else {
+    return (
+        <>
+          <Nav></Nav>
+          <h1 style={{ marginTop: "10rem", textAlign: "center", fontSize: "4rem" }}>
+            SHOP
+          </h1>
+          <div
+            className="shop-items-container"
+            style={{
+              display: "grid",
+              gridTemplateColumns: "repeat(4, 1fr)",
+              gridTemplateRows: "repeat(4, 1fr)",
+              gap: "2rem",
+            }}
+          >
+            <img src={itemData[0].image} alt="" />
+          </div>
+        </>
+      );
+  }
 }
 
 export default Shop;
